36bf04cc378c0f4abadfdc4f124f1b405ab52ceb
Adrien Meltzer <ameltzer@elium-web.com>
2017-06-23 19:41:22 +0200

    TestUnitaire KeyGen V1

---
+++ Proof Of Concept/KeyGeneratorTests/KeyByteTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KeyGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyGenerator.Tests
+{
+    [TestClass()]
+    public class KeyByteTests
+    {
+
+        [TestMethod()]
+        public void SizeKeyTest()
+        {
+            KeyByte key = new KeyByte(32);
+
+            string str = key.GetKey();
+
+            Assert.AreEqual(4, str.Length);
+        }
+
+        [TestMethod()]
+        public void DifferentIncrementKeyTest()
+        {
+            KeyByte key = new KeyByte(32);
+
+            string str1 = key.GetKey();
+            string str2 = key.GetKey();
+
+            Assert.AreNotEqual(str1, str2);
+        }
+    }
+}
--- Proof Of Concept/KeyGeneratorTests/Properties/AssemblyInfo.cs
+++ Proof Of Concept/KeyGeneratorTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("KeyGeneratorTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("KeyGeneratorTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("5a38efed-c234-4a75-b25f-d4b39a9fb419")]
+[assembly: Guid("dee3a86d-f286-4eb5-8eda-c9a059364cd7")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
