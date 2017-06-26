f8eff5f1f8a3e47a2bd14716b0987564b44d31d6
Adrien Meltzer <adrien.meltzer@viacesi.fr>
2017-06-26 11:39:43 +0200

    Install Authentification System (70% finish)

--- C-Sharp/Client/MainWindow.xaml.cs
+++ C-Sharp/Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Client.FrontServiceClient;
 
 namespace Client
 {
@@ -23,6 +24,12 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DecryptageServiceClient client = new DecryptageServiceClient();
+            MessageBox.Show(client.GetText());
         }
     }
 }
---
+++ C-Sharp/FrontWCFService/FrontService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FrontWCFService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class FrontService : IDecryptageService
+    {
+        public string GetText()
+        {
+            return "Hello World!";
+        }
+    }
+}
